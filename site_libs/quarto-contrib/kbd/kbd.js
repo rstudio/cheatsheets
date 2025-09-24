a04a36e6ce8abe2eda37f67c28a492f92209cbbb
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-09-24 13:35:18 +0000

    Built site for gh-pages

--- site_libs/quarto-contrib/kbd/kbd.js
+++ site_libs/quarto-contrib/kbd/kbd.js
@@ -17,6 +17,30 @@
   }
   const os = guessOS();
 
+  const macosModifiers = {
+    "command": "⌘",
+    "cmd": "⌘",
+    "shift": "⇧",
+    "ctrl": "⌃",
+    "control": "⌃",
+    "option": "⌥",
+  };
+  const keyRegex = /^[^\s]+$/;
+    // use a non-capturing group to avoid capturing the key names
+
+  const macosModifiersRegex = new RegExp(
+    // use a non-capturing group to avoid capturing the key names
+    `^(?:${Object.keys(macosModifiers).join("|")})-`,
+    "gi"
+  );
+  const shortcutParses = (text) => {
+    text = text.toLocaleLowerCase();
+    while (text.match(macosModifiersRegex)) {
+      text = text.replace(macosModifiersRegex, "");
+    }
+    return text.match(keyRegex) !== null;
+  }
+
   // deno-lint-ignore no-window-prefix
   window.addEventListener("DOMContentLoaded", (_) => {
     for (const el of Array.from(document.querySelectorAll("kbd"))) {
@@ -24,14 +48,12 @@
       if (el.dataset[os.name] !== undefined) {
         el.innerText = el.dataset[os.name];
       }
-      if (os.name === "mac") {
-        el.innerText = el.innerText
-          .replaceAll(/command-?/gi, "⌘")
-          .replaceAll(/cmd-?/gi, "⌘")
-          .replaceAll(/shift-?/gi, "⇧")
-          .replaceAll(/ctrl-?/gi, "⌃")
-          .replaceAll(/control-?/gi, "⌃")
-          .replaceAll(/option-?/gi, "⌥");
+      if (os.name === "mac" && shortcutParses(el.innerText)) {
+        el.classList.add("mac");
+        for (const [key, value] of Object.entries(macosModifiers)) {
+          el.innerText = el.innerText.replaceAll(new RegExp(`${key}-`, "gi"), value);
+        }
+        el.innerText = el.innerText.toLocaleUpperCase();
       }
     }
   });
